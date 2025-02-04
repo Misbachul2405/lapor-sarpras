040de46153899e278ba093da563bbae18ef26cab
Misbachul2405 <vicki240593@gmail.com>
2025-02-04 20:49:37 +0700

    penambahan Role and Permission

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        user::create ([
+            'name' => 'Admin Lapor Sarpras',
+            'email' => 'vicki240593@gmail.com',
+            'password' => bcrypt('admin12345')
+
+        ])->assignRole('admin');
+    }
+}
